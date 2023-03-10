ec95bd99365d6d344687b852d87e29b301d991ef
gaurav <gauravp@zignuts.com>
2023-03-10 13:02:08 +0530

    formatting

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,7 +21,12 @@ class StudentController extends Controller
             } else
                 $students = Student::paginate(15);
         } else {
-            $students = Student::where('name', 'like', '%' . $search . '%')->orWhere('city', 'like', '%' . $search . '%')->orWhere('phone', 'like', '%' . $search . '%')->orWhere('email', 'like', '%' . $search . '%')->orWhere('id', $search)->paginate(15);
+            $students = Student::where('name', 'like', '%' . $search . '%')
+                ->orWhere('city', 'like', '%' . $search . '%')
+                ->orWhere('phone', 'like', '%' . $search . '%')
+                ->orWhere('email', 'like', '%' . $search . '%')
+                ->orWhere('id', $search)
+                ->paginate(15);
         }
         return view('student', compact('students'));
     }
@@ -51,7 +56,13 @@ class StudentController extends Controller
         $orderBy = $request['orderBy'] ? $request['orderBy'] : 'name';
 
         if ($search) {
-            $students = Student::where('name', 'like', '%' . $search . '%')->orWhere('city', 'like', '%' . $search . '%')->orWhere('phone', 'like', '%' . $search . '%')->orWhere('email', 'like', '%' . $search . '%')->orWhere('id', $search)->orderBy($orderBy, $inOrder)->paginate($perpage);
+            $students = Student::where('name', 'like', '%' . $search . '%')
+                ->orWhere('city', 'like', '%' . $search . '%')
+                ->orWhere('phone', 'like', '%' . $search . '%')
+                ->orWhere('email', 'like', '%' . $search . '%')
+                ->orWhere('id', $search)
+                ->orderBy($orderBy, $inOrder)
+                ->paginate($perpage);
 
             if (sizeof($students) == 0) {
                 return $this->returnResponse('false', 'No Record Found');
